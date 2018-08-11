fdd787a3e1274b904726e607fdeefeaf71a11137
Emilio Ramon GUMAYAGAY <emilio.gumayagay@tap-group.com.cn>
2018-08-11 17:50:39 +0800

    finished course project- the basics

---
+++ src/app/recipe/recipe-list/recipe-list.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
+
+
+@Component({
+  selector: 'app-recipe-list',
+  templateUrl: './recipe-list.component.html',
+  styleUrls: ['./recipe-list.component.css']
+})
+export class RecipeListComponent implements OnInit {
+
+  recipes: Recipe[] = [
+    new Recipe('Test Recipe',
+          'This is simply a test',
+        'http://img.bestrecipes.com.au/RCK3slSo/h300-w400-cscale/br-api/asset/20771/super-easy-pizza-dough-recipe.jpg'),
+        new Recipe('Test Recipe',
+          'This is simply a test',
+        'http://img.bestrecipes.com.au/RCK3slSo/h300-w400-cscale/br-api/asset/20771/super-easy-pizza-dough-recipe.jpg')
+  ]; //array type recipes from Recipe Model
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
